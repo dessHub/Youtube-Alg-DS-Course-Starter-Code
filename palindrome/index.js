e41312e1c2fd3f7a304a3ed4a4fc52828c2b6dfc
desshub <korirdesmond@MacBook-Pro-3.local>
2020-06-18 19:50:59 +0300

    Implement palindrome

--- palindrome/index.js
+++ palindrome/index.js
@@ -5,7 +5,11 @@
 //   palindrome("Madam") === true
 //   palindrome("love") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  str = str.toLowerCase();
+  const reversedStr = str.split('').reverse().join('');
+  return reversedStr === str;
+}
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \

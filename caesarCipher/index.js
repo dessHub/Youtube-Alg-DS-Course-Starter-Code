9845375fac3e011cc00f63403b49d735adf2907d
desshub <korirdesmond@MacBook-Pro-3.local>
2020-08-01 22:19:35 +0300

    CaesarCipher

--- caesarCipher/index.js
+++ caesarCipher/index.js
@@ -8,7 +8,25 @@
 // caeserCipher("abcd", 100) === "wxyz";
 // caeserCipher("gurer ner 9 qbtf!", 13) === "there are 9 dogs!"
 
-function caesarCipher(str, shift) {}
+function caesarCipher(str, shift) {
+  const alphabetArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  let res = "";
+
+  for(let i = 0; i < str.length; i++) {
+    const char = str[i];
+    const idx = alphabetArr.indexOf(char);
+
+    if(idx === -1) {
+      res += char;
+      continue
+    }
+
+    const encodedIdx = (idx + shift) % 26;
+    res += alphabetArr[encodedIdx];
+  }
+
+  return res;
+}
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
